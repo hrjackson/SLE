5f130875f9e864e549286648c1b11e99e51012dd
Henry Jackson <hrjackson@gmail.com>
2015-02-10 15:13:23 +0000

    Initial Commit

---
+++ SLE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SLE
+//
+//  Created by Henry Jackson on 10/02/2015.
+//  Copyright (c) 2015 n/a. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
