40093999077319600cd586c4268b42ee8be978fc
Henry Jackson <hrjackson@gmail.com>
2015-03-18 17:29:33 +0000

    Writing stochastic process definitions and implementation

--- SLE/stochastic_processes.cpp
+++ SLE/stochastic_processes.cpp
@@ -7,3 +7,49 @@
 //
 
 #include "stochastic_processes.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//// Helper function definitions ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+int indexAbove(std::vector<double>, double time);
+std::vector<std::vector<double> > insert(std::vector<std::vector<double> > vec,
+                                         int index,
+                                         std::vector<double>);
+
+
+////////////////////////////////////////////////////////////////////////////////
+//// Process member function definitions ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+Process::Process(std::vector<double> initial_pos){
+    std::vector<double> times(1, 0.0);
+    std::vector<std::vector<double> > values;
+    values.push_back(initial_pos);
+    dimension = initial_pos.size();
+};
+
+void Process::setValue(double time, std::vector<double> position){
+    auto elem = std::max_element(times.begin(), times.end());
+    if (time > *elem) {
+        times.push_back(time);
+        values.push_back(position);
+    }
+    else {
+        int index = indexAbove(times, time);
+        times.insert(times.begin()+index, time);
+        values.insert(values.begin()+index, position);
+    }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+//// Helper function implementations ///////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Finds the index a time should be inserted into a sorted vector of times
+int indexAbove(std::vector<double> sortedVec, double time){
+    int index = 0;
+    while (sortedVec.at(index) < time) {
+        index++;
+    }
+    return index;
+};
--- SLE/stochastic_processes.h
+++ SLE/stochastic_processes.h
@@ -10,5 +10,26 @@
 #define __SLE__stochastic_processes__
 
 #include <stdio.h>
+#include <vector>
+
+class Process {
+protected:
+    std::vector<double> times;
+    std::vector<std::vector<double> > values;
+    unsigned long dimension;
+public:
+    Process(std::vector<double>);
+    void setValue(double time, std::vector<double> position);
+};
+
+class StochasticProcess : public Process {
+public:
+    std::vector<double> getValue(double time);
+};
+
+class BrownianMotion : public Process {
+public:
+    std::vector<double> getValue(double time);
+};
 
 #endif /* defined(__SLE__stochastic_processes__) */
