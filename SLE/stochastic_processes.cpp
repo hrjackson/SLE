9362f50a2fbc4666b884f1b278d77efa950de9aa
Henry Jackson <hrjackson@gmail.com>
2015-03-18 13:36:49 +0000

    Setup, and test that its working

---
+++ SLE/stochastic_processes.cpp
@@ -0,0 +1,9 @@
+//
+//  stochastic_processes.cpp
+//  SLE
+//
+//  Created by Henry Jackson on 18/03/2015.
+//  Copyright (c) 2015 n/a. All rights reserved.
+//
+
+#include "stochastic_processes.h"
---
+++ SLE/stochastic_processes.h
@@ -0,0 +1,14 @@
+//
+//  stochastic_processes.h
+//  SLE
+//
+//  Created by Henry Jackson on 18/03/2015.
+//  Copyright (c) 2015 n/a. All rights reserved.
+//
+
+#ifndef __SLE__stochastic_processes__
+#define __SLE__stochastic_processes__
+
+#include <stdio.h>
+
+#endif /* defined(__SLE__stochastic_processes__) */
